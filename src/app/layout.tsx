6880bcad942bbe98a1e22fa261de50d71aa13340
WHITEGUARD DEV TEAM <ahmedmostafakhedr31@gmail.com>
2026-06-09 11:41:12 +0300

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
+import {
+  GoogleTagManagerNoScript,
+  GoogleTagManagerScript,
+} from "@/components/analytics/GoogleTagManager";
 import { ScrollToTopButton } from "@/components/ui/ScrollToTopButton";
 import "./globals.css";
 
@@ -36,9 +40,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="0lqEsKlzYOHsMmUoI2XErolOme7kJb-SW0IzrECZRUA"
+        />
+        <GoogleTagManagerScript />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${plusJakartaSans.variable} antialiased`}
       >
+        <GoogleTagManagerNoScript />
         {children}
         <ScrollToTopButton />
       </body>
